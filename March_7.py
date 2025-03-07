731dbf151cdd4e3be7da1f94caeeb03d9d0d91f5
Shivi-pixel-740 <ShivangniS582@stemcollegiate.ca>
2025-03-07 11:50:12 -0500

    Create March_7.py

---
+++ March_7.py
@@ -0,0 +1,15 @@
+#I learned taking user input and printing a variable
+
+Yourname = input("What is your name?")
+Hungry = input ("What is your favorite food?")
+Music = input("What is your favorite music?")
+print()
+print("You are")
+print(Yourname)
+print()
+
+print("You're probably hungry for")
+print(Hungry)
+print()
+print("and you're definitely getting your groove on to")
+print(Music)
