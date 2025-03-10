38b2c6e680a44559c40fa79870c9772ca250746b
Shivi-pixel-740 <ShivangniS582@stemcollegiate.ca>
2025-03-10 14:28:59 -0400

    Create Lesson 3: March_10.py

---
+++ Lesson 3: March_10.py
@@ -0,0 +1,12 @@
+#I learned how to concatenate and understand my errors
+
+food = input ( "What is your favorite food? ")
+plant = input ( "What is your favorite plant? ")
+cooking = input ( "What is your favorite method of cooking? ")
+
+
+burned = input ( "What is a word to describe burned food? ")
+item = input ( "What is your favorite household item? ")
+
+print ("MENU")
+print (cooking, food,"is a great way to use the food",burned,"is unfortunate", plant,"helps to heal", item, "is a great item to use")
