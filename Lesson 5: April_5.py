6539d7b1219ba0321cbb584dbab2df8a6d370fe0
Shivi-pixel-740 <ShivangniS582@stemcollegiate.ca>
2025-04-06 01:53:14 -0400

    Create Lesson 5: April_5.py

---
+++ Lesson 5: April_5.py
@@ -0,0 +1,24 @@
+#I learned the If statments and that double equals (==) is asking the computer to compare if these two things are exactly the same. 
+
+TypesFood = input("Do you prefer salad or icecream?")
+if TypesFood == "salad": 
+  
+    print("Great choice!, salad is healthy")
+  
+else:
+    print("Icecream is disgusting")
+          
+Food = input("What is your favorite food?")
+if Food == "greek salad":
+  
+    print("Nice choice")
+else:
+    print("I don't like that food")
+
+BestFOOD = input("What is your best food?")
+if BestFOOD == "fruit salad":
+  
+    print("You are a Greek God")
+else:
+    print("I don't like that food")
+    print("You must be healthy")
